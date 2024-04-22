c376a99967169f33f4ac0049b88f1624340bbe1c
sracem <s.racem@coconsult.fr>
2024-04-22 22:47:35 +0200

    First commit

---
+++ src/main/java/com/racem/driveanddeliver/DriveanddeliverApplication.java
@@ -0,0 +1,13 @@
+package com.racem.driveanddeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveanddeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveanddeliverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/racem/driveanddeliver/DriveanddeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.racem.driveanddeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveanddeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
